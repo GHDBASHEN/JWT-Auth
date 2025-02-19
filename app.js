ee41d7ccde0d695f7e013994e6450433d4b2b641
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-02-19 21:34:28 +0530

    Refactor app.js to implement user registration and login functionality with JWT authentication

--- app.js
+++ app.js
@@ -1,41 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// app.js 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
+const PORT = process.env.PORT ||3000;
+const SECRET_KEY = 'xxxx-xxxx';
 
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// This will act as our 'database'
+let users = [];
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Register route
+async function register(username, email, password) {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 8);
+    // Save the user
+    users.push({ username, password: hashedPassword, email });
+    console.log('User registered Successfully.');
+    return true;
+}
+
+// Login route
+async function login(email, password) {
+    // Find the user
+    const user = users.find(user => user.email == email);
+    if (!user) {
+        console.log('User not found.')
+        return null;
+    }
+    // Check the password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        console.log('Invalid credentials');
+        return null;
+    }
+    console.log('User Details', user, '\n')
+    // Generate a JWT
+    const token = jwt.sign(
+        { email },
+        SECRET_KEY,
+        { expiresIn: '1h' });
+    console.log('Token', token, '\n')
+    return token;
+}
+
+// register a user 
+register('Sandeep', 'ex@gmail.com', 'exm123')
+
+setTimeout(() => {
+    login('ex@gmail.com', 'exm123')
+}, 5000); // after 5 second login 
+
+app.listen(PORT, () => {
+    console.log(`Server is running  ${PORT}`);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
--- server.js
+++ server.js
@@ -1,70 +1,70 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-const port = process.env.PORT;
-const app = express();
-//const SECRET_KEY = process.env.SECRET_KEY;
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
-});
+// import express from 'express';
+// import jwt from 'jsonwebtoken';
+// const port = process.env.PORT;
+// const app = express();
+// //const SECRET_KEY = process.env.SECRET_KEY;
+// app.get('/', (req, res) => {
+//     res.json({ message: 'Hello World!' });
+// });
 
 
-app.post('/login', (req, res) => {
+// app.post('/login', (req, res) => {
 
-    //req--->database --->true
-    //generate token
-    //sign userdata
+//     //req--->database --->true
+//     //generate token
+//     //sign userdata
 
-    var userdata = { id: 2, username: "janith", age: 20 };
-    jwt.sign({ user: userdata }, process.env.SECRET_KEY, (err, token) => {
-        if (err) {
-            res.json({ error: err })
-        }
-        else {
-            res.json({ token: token })
-        }
-    })
-
-
-})
+//     var userdata = { id: 2, username: "janith", age: 20 };
+//     jwt.sign({ user: userdata }, process.env.SECRET_KEY, (err, token) => {
+//         if (err) {
+//             res.json({ error: err })
+//         }
+//         else {
+//             res.json({ token: token })
+//         }
+//     })
 
 
-app.post('/verify', verifyToken, (req, res) => {
+// })
 
-    //check valid user 
+
+// app.post('/verify', verifyToken, (req, res) => {
+
+//     //check valid user 
     
-    jwt.verify(req.token, process.env.SECRET_KEY, (err,data) => {
-        if (err) {
-            res.json({ msg: "Access Denied" })
-        }
-        else {
-            res.json({ msg:"Data Saved",data:data })
-        }
-    })
-})
+//     jwt.verify(req.token, process.env.SECRET_KEY, (err,data) => {
+//         if (err) {
+//             res.json({ msg: "Access Denied" })
+//         }
+//         else {
+//             res.json({ msg:"Data Saved",data:data })
+//         }
+//     })
+// })
 
 
 
 
-function verifyToken(req, res, next) {
-    if (typeof req.headers['authorization'] !== 'undefined' && req.headers['authorization'] !== 'undefined') {
+// function verifyToken(req, res, next) {
+//     if (typeof req.headers['authorization'] !== 'undefined' && req.headers['authorization'] !== 'undefined') {
 
-        var HeaderToken = req.headers['authorization'].split(' ')[1];
-        if (HeaderToken !== 'undefined') {
-            req.token = HeaderToken;
-            next();
-        }
-        else {
-            res.json({ message: "Unauthorized Request" });
-        }
+//         var HeaderToken = req.headers['authorization'].split(' ')[1];
+//         if (HeaderToken !== 'undefined') {
+//             req.token = HeaderToken;
+//             next();
+//         }
+//         else {
+//             res.json({ message: "Unauthorized Request" });
+//         }
 
-    }
-    else {
-        res.json({ message: "Unauthorized Request" });
-    }
+//     }
+//     else {
+//         res.json({ message: "Unauthorized Request" });
+//     }
 
-}
+// }
 
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
